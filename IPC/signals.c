5718e0ae8d94ce4cbfdca933ade4394f46c95534
Arun PM <arunpm@gmail.com>
2017-12-27 16:19:59 +0530

    Linux: IPC comminication examples

---
+++ IPC/signals.c
@@ -0,0 +1,56 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <signal.h>
+
+/* Sungal handlers */
+void custom_sighup(int sig)
+{
+	signal(SIGHUP, custom_sighup); /* reset signal */
+	printf("CHILD: I have received a SIGHUP\n");
+}
+
+void custom_sigint(int sig)
+{
+	signal(SIGINT, custom_sigint); /* reset signal */
+	printf("CHILD: I have received a SIGINT\n");
+}
+
+void custom_sigquit(int sig)
+{
+	printf("Quit signal received; exiting...!!!\n");
+	exit(0);
+}
+
+
+int main(void)
+{
+	int pid;
+
+	/* get child process */
+	if ((pid = fork()) < 0) {
+		perror("fork");
+		exit(1);
+	}
+
+	if (pid == 0) {
+		/* child */
+		signal(SIGHUP, custom_sighup); /* set function calls */
+		signal(SIGINT, custom_sigint);
+		signal(SIGQUIT, custom_sigquit);
+		for(;;); /* loop for ever */
+	} else {
+		/* Parent */
+		/* pid hold id of child */
+		printf("\nPARENT: sending SIGHUP\n\n");
+		kill(pid, SIGHUP);
+		sleep(3); /* pause for 3 secs */
+		printf("\nPARENT: sending SIGINT\n\n");
+		kill(pid, SIGINT);
+		sleep(3); /* pause for 3 secs */
+		printf("\nPARENT: sending SIGQUIT\n\n");
+		kill(pid, SIGQUIT);
+		sleep(1);
+	}
+}
+
--- README.md
+++ README.md
@@ -1,2 +1,12 @@
 # Linux
-This repo contains some example codes intended to run on linux platform.
+This repo contains some example codes intended to run on linux platform.  
+
+###### IPC
+A collection of sample C programs to illustrate Inter process communication mechanism.  
+- Signals  
+- Message Queues  
+- Semaphores  
+- Shared Memory  
+- Sockets  
+
+
